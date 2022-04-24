f60c2f51eb61d4731d053fc834946cdc692a2626
Alexandr Tultsov <tultsov@inbox.ru>
2022-04-24 12:07:51 +0200

    Добавлена функция TimSort и функция вывода массива

--- TimSort.java
+++ TimSort.java
@@ -2,14 +2,11 @@
 
 class trivial{
 
-    int block = 32; // размер блока
-
     public static void inSort(int arr[],int left, int right){ // сортировка вставками
         for (int i = left + 1; i < right; i++){
             int temp = arr[i];
         int j = i - 1;
-        while (arr[j] > temp && j >= left)
-        {
+        while (arr[j] > temp && j >= left){
             arr[j + 1] = arr[j];
             j--;
         }
@@ -17,53 +14,70 @@ class trivial{
         }
     }
     public static void merge(int arr[], int l, int m, int r){  // функция слияния объединяет отсортированные блоки
-        // исходный массив разбит на две части
-    // левый и правый массив
-    int len1 = m - l + 1, len2 = r - m;
-    int left[len1], right[len2];
-    for (int i = 0; i < len1; i++)
-        left[i] = arr[l + i];
-    for (int i = 0; i < len2; i++)
-        right[i] = arr[m + 1 + i];
-    int i = 0;
-    int j = 0;
-    int k = l;
-    // после сравнения мы объединяем эти два массива
-    // в большем подмассиве
-    while (i < len1 && j < len2)
-    {
-        if (left[i] <= right[j])
-        {
+        int len1 = m - l + 1, len2 = r - m;         // исходный массив разбит на две части
+        int left[len1], right[len2];
+        for (int i = 0; i < len1; i++)
+            left[i] = arr[l + i];
+        for (int i = 0; i < len2; i++)
+            right[i] = arr[m + 1 + i];
+        int i = 0;
+        int j = 0;
+        int k = l;
+        while (i < len1 && j < len2){
+            if (left[i] <= right[j]){
+                arr[k] = left[i];
+                i++;
+            }
+            else{
+            arr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < len1){ // копируем оставшиеся элементы left, если есть
             arr[k] = left[i];
+            k++;
             i++;
         }
-        else
-        {
+        while (j < len2){ // копируем оставшийся элемент right, если есть
             arr[k] = right[j];
+            k++;
             j++;
         }
-        k++;
     }
-    while (i < len1) // копируем оставшиеся элементы left, если есть
-    {
-        arr[k] = left[i];
-        k++;
-        i++;
-    }
-    while (j < len2) // копируем оставшийся элемент right, если есть
-    {
-        arr[k] = right[j];
-        k++;
-        j++;
-    }
-}
-
-
     
+    public static void TimSort(int arr[], int n){
+        int block = 32; // итеративная функция Timsort для сортировки массива [0 ... n-1] (аналог сортировки слиянием)
+        for (int i = 0; i < n; i += RUN) // Сортировка отдельных подмассивов по размеру блока
+            insertionSort(arr, i, min((i + 31), (n - 1)));
+        for (int size = block; size < n; size = 2 * size){ // начинаем слияние с размера блоrа (или 32). Будет сливаться сформировать размер 64, затем 128, 256 и т. д.
+        // Берем начальную точку левого подмассива. Мы
+        // собираемся объединить arr [left..left + size-1]
+        // и arr [left + size, left + 2 * size-1]
+        // После каждого слияния мы увеличиваем left на 2 * размер
+        for (int left = 0; left < n; left += 2 * size){
+            // находим конечную точку левого подмассива
+            // mid + 1 - начальная точка правого подмассива
+            int mid = left + size - 1;
+            int right = min((left + 2 * size - 1), (n - 1));
+            // объединяем подмассив arr [left ..... mid] &
+            // arr [mid + 1 .... right]
+            merge(arr, left, mid, right);
+            }
+        }
+    }
+
+    public static void  printarr(int arr[]){
+        for (int i = 0; i < 11; i++)
+            System.out.print(arr[i] + " ");
+    }
+
     public static void main(String args[]){
         int arr[] = {1,4,3,3,2,6,7,8,4,3,1};
-        for (int i = 0; i < 11; i++){
-            System.out.print(arr[i] + " ");
-        }
+        System.out.print("Начальный массив");
+        printarr(arr);
+        TimSort(arr);
+        System.out.print("Отсоритрованный массив");
+        printarr(arr);
     }
 }
